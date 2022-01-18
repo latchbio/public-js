8d07c3ae9f280ea8b7d106aa6c2be1cc99a4e7db
Maksim Smolin <maximsmol@gmail.com>
2022-01-18 10:12:41 -0800

    feat: port array parsing

--- packages/scsv-parser/src/types.ts
+++ packages/scsv-parser/src/types.ts
@@ -41,3 +41,79 @@ export type SCSVType =
   | SCSVObject
   | SCSVRecord
   | SCSVUnion;
+
+export const scsv = {
+  string: {
+    type: SCSVPrimitiveName.string,
+  },
+  number: {
+    type: SCSVPrimitiveName.number,
+  },
+  boolean: {
+    type: SCSVPrimitiveName.boolean,
+  },
+  null: {
+    type: SCSVPrimitiveName.null,
+  },
+  array(elementType: SCSVType): SCSVArray {
+    return {
+      type: "array",
+      elementType,
+    };
+  },
+  tuple(...elements: SCSVType[]): SCSVTuple {
+    return {
+      type: "tuple",
+      elements,
+    };
+  },
+  object(valueType: SCSVType): SCSVObject {
+    return {
+      type: "object",
+      valueType,
+    };
+  },
+  record(fields: Record<string, SCSVType>): SCSVRecord {
+    return {
+      type: "record",
+      fields,
+    };
+  },
+  union(...variants: SCSVType[]): SCSVUnion {
+    return {
+      type: "union",
+      variants,
+    };
+  },
+};
+
+export type SCSVOutput =
+  | string
+  | number
+  | boolean
+  | null
+  | SCSVOutput[]
+  | { [key: string]: SCSVOutput };
+
+// todo(maximsmol): implement this properly
+// export type JSTypeFromSCSVType<T extends SCSVType> = T extends SCSVPrimitive
+//   ? T["type"] extends SCSVPrimitiveName.string
+//     ? string
+//     : T["type"] extends SCSVPrimitiveName.number
+//     ? number
+//     : T["type"] extends SCSVPrimitiveName.boolean
+//     ? boolean
+//     : T["type"] extends SCSVPrimitiveName.null
+//     ? null
+//     : never
+//   : T extends SCSVArray
+//   ? JSTypeFromSCSVType<T["elementType"]>[]
+//   : T extends SCSVTuple
+//   ? { [K in keyof T["elements"]]: SCSVType }
+//   : T extends SCSVObject
+//   ? { [key: string]: JSTypeFromSCSVType<T["valueType"]> }
+//   : T extends SCSVRecord
+//   ? { [K in keyof T["fields"]]: JSTypeFromSCSVType<T["fields"][K]> }
+//   : T extends SCSVUnion
+//   ? keyof { [K in keyof T["variants"]]: SCSVType }
+//   : never;
